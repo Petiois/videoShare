2b73e469e4305e7006a4cc1ef903b3a25bcebd9c
camille <codec_abc@hotmail.fr>
2012-10-27 15:48:40 +0200

    working on design using bootstrap

--- videoShare/views.py
+++ videoShare/views.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
@@ -6,9 +9,10 @@ from django.core.urlresolvers import reverse
 from videoShare.models import Document
 from videoShare.forms import DocumentForm
 from django.http import HttpResponse
+from hashlib import sha256
 
 def home(request):
-   return HttpResponse("Home Page")
+    return render_to_response('home.html')
 
 def isLog(request):
    login = request.user.username
@@ -24,6 +28,12 @@ def list(request):
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             newdoc = Document(docfile = request.FILES['docfile'])
+            m = sha256()
+            m
+            m.update(newdoc.docfile.read())
+            newdoc.docfile.name = m.hexdigest()
+            newdoc.save()
+
             # Redirect to the document list after POST
             return HttpResponseRedirect(reverse('videoShare.views.list'))
     else:
